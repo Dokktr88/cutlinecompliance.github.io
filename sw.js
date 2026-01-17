7bc3ef339d7762260a9a6881fe8b0e5861fc0f5f
Dokktr88 <jesse.stanley@cutlinecompliance.com>
2026-01-17 01:09:31 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,25 @@
-const CACHE = "cutline-v2";
-
+const CACHE = "cutline-v4";
 const ASSETS = [
   "/",
   "/index.html",
   "/styles.css",
-  "/manifest.webmanifest",
   "/youtube.html",
+  "/manifest.webmanifest",
   "/cutline-open-512.png",
   "/favicon-32.png",
   "/favicon.ico",
+  "/Cutline_text_1.png",
+  "/Cutline_text_2.png",
+  "/Blue Shirt White Background3v3.jpg",
+  "/IMG_9489.PNG",
   "/IMG_9490.PNG",
   "/IMG_9491.PNG",
   "/IMG_9492.PNG",
-  "/IMG_9499.PNG",
-  "/IMG_9489.PNG"
+  "/IMG_9499.PNG"
 ];
 
 self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open(CACHE)
-      .then((c) => c.addAll(ASSETS))
-      .catch(() => {})
-  );
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
 });
 
 self.addEventListener("activate", (e) => {
@@ -30,6 +28,7 @@ self.addEventListener("activate", (e) => {
       Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
     )
   );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", (e) => {
